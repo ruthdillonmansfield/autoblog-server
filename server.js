953161cf5793bd2aee14db611c0a005f2196ca3b
Ruth Ng <ruth.ym.ng@gmail.com>
2023-04-21 15:48:27 +0100

    Move to filesystem and add CORS

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const OpenAI = require('openai');
 const cron  = require('node-cron');
 const express  = require('express');
-
-
-const { Octokit } = require('@octokit/rest');
-const octokit = new Octokit({
-    auth: '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',
-    userAgent: 'autoBlog v1.0.0',
-});
+const cors  = require('cors');
+const fs  = require('fs');
 
 // Define function to generate and save a new blog post
 const generateBlogPost = async () => {
@@ -60,71 +55,54 @@ cron.schedule('0 0 * * *', () => {
 // Create Express app
 const app = express();
 
+app.use(cors());
+app.use('/blogs', express.static('blogs'));
+
+// Read the blog index file to get the mapping of index numbers to blog names
+const blogIndexData = fs.readFileSync('blogs/blog-index.json');
+const blogIndex = JSON.parse(blogIndexData);
+
 // Define route to retrieve all blog posts
-app.get('/blog-posts', async (req, res) => {
-    try {
-      // Get all blog post file names from GitHub
-      const { data } = await octokit.repos.getContent({
-        owner: 'ruthdillonmansfield',
-        repo: 'autoblog-contents',
+app.get('/blog-posts', (req, res) => {
+  try {
+    // Get the contents of all JSON files in the blogs directory
+    const files = fs.readdirSync('blogs');
+    const blogPosts = files
+      .filter(file => file.endsWith('.json'))
+      .map(file => {
+        const fileContents = fs.readFileSync(`blogs/${file}`);
+        return JSON.parse(fileContents);
       });
-  
-      // Filter out any non-JSON files and get their contents
-      const promises = data.filter((file) => file.name.endsWith('.json')).map(async (file) => {
-        const { data } = await octokit.repos.getContent({
-          owner: 'ruthdillonmansfield',
-          repo: 'autoblog-contents',
-          path: file.path,
-        })
-  
-        // Parse the contents of the file as JSON
-        const blogPost = JSON.parse(Buffer.from(data.content, 'base64').toString('utf8'));
-  
-        return blogPost;
-      });
-  
-      // Wait for all promises to resolve and combine the results into a single array
-      const blogPosts = await Promise.all(promises);
-  
-      if (blogPosts.length > 0) {
-        res.json(blogPosts);
-      } else {
-        res.status(404).send('No blog posts found.');
-      }
-    } catch (error) {
+    if (blogPosts.length > 0) {
+      res.json(blogPosts);
+    } else {
+      res.status(404).send('No blog posts found.');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error.');
+  }
+});
+
+// Define route to retrieve a specific blog post by ID
+app.get('/blog-posts/:id', (req, res) => {
+  const id = req.params.id;
+  try {
+    // Get the contents of the blog post file
+    const fileContents = fs.readFileSync(`blogs/${id}.json`);
+    const blogPost = JSON.parse(fileContents);
+
+    // Send the blog post as a response
+    res.json(blogPost);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      res.status(404).send('Blog post not found.');
+    } else {
       console.error(error);
       res.status(500).send('Internal server error.');
     }
+  }
 });
-  
-  
-// Define route to retrieve a specific blog post by ID
-app.get('/blog-posts/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-      // Get the contents of the blog post file
-      const { data } = await octokit.repos.getContent({
-        owner: 'ruthdillonmansfield',
-        repo: 'autoblog-contents',
-        path: `${id}.json`,
-      });
-      const fileContents = Buffer.from(data.content, 'base64').toString('utf8');
-  
-      // Parse the contents of the file as JSON
-      const blogPost = JSON.parse(fileContents);
-  
-      // Send the blog post as a response
-      res.json(blogPost);
-    } catch (error) {
-      if (error.status === 404) {
-        res.status(404).send('Blog post not found.');
-      } else {
-        console.error(error);
-        res.status(500).send('Internal server error.');
-      }
-    }
-});
-  
 
 // Start the server
 app.listen(3000, () => {
