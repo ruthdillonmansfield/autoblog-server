8158952438da661eaacff7adc1d1396a17427d43
Ruth Ng <ruth.ym.ng@gmail.com>
2023-04-23 17:56:20 +0100

    Generates blogs with OpenAI API and uses Octokit to add them to front end

--- server.js
+++ server.js
@@ -1,110 +1,145 @@
-const OpenAI = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 const cron  = require('node-cron');
 const express  = require('express');
 const cors  = require('cors');
-const fs  = require('fs');
+const dotenv = require('dotenv');
+const { Octokit } = require('@octokit/core');
 
-// Define function to generate and save a new blog post
-const generateBlogPost = async () => {
+dotenv.config();
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const octokit = new Octokit({ auth: process.env.OCTOKIT_KEY });
+
+const repoOwner = 'ruthdillonmansfield';
+const repoName = 'autoblog_frontend';
+const branch = 'main';
+
+const app = express();
+app.use(cors());
+
+async function generateAndSaveBlogPost() {
   try {
-    // Get the last 20 blog posts from GitHub
-    const { data } = await octokit.repos.getContent({
-      owner: 'ruthdillonmansfield',
-      repo: 'autoblog-contents',
-    //   path: '/',
-    });
-    const blogPosts = JSON.parse(Buffer.from(data.content, 'base64').toString('utf8')).slice(0, 20);
-    const prompts = blogPosts.map((post) => post.topic);
-
-    // Use the OpenAI API to generate a new unique blog post based on the last 20 blog post topics
-    const response = await OpenAI.completions.create({
-      model: 'text-davinci-002',
-      prompt: `Here are the last 20 blog posts:\n\n${prompts.join('\n')}\n\nWrite a new blog post that is unique and interesting.`,
-      maxTokens: 1024,
-      n: 1,
+    // Fetch the last 20 blog titles from the front-end repository
+    // Fetch the last 20 blog titles from the front-end repository
+    const listFilesResponse = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+      owner: repoOwner,
+      repo: repoName,
+      path: 'src/blogs',
+      ref: branch,
     });
 
-    // Save the generated blog post to GitHub
-    const newBlogPost = {
-      topic: response.choices[0].text,
-      content: response.choices[0].text,
-      created_at: new Date().toISOString(),
-    };
-    blogPosts.unshift(newBlogPost);
-    const fileContent = JSON.stringify(blogPosts, null, 2);
-    await octokit.repos.createOrUpdateFileContents({
-      owner: 'your-username',
-      repo: 'your-repository',
-      path: '/',
-      message: 'Add new blog post',
-      content: Buffer.from(fileContent).toString('base64'),
-      sha: data.sha,
+    const last20Files = listFilesResponse.data.slice(-20);
+
+    // Fetch the content of each file and parse the JSON to get the titles
+    const last20TitlesPromises = last20Files.map(async (file) => {
+      const fileContentResponse = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+        owner: repoOwner,
+        repo: repoName,
+        path: file.path,
+        ref: branch,
+      });
+
+      const fileContent = Buffer.from(fileContentResponse.data.content, 'base64').toString();
+      const json = JSON.parse(fileContent);
+      return json.title;
     });
 
-    console.log('Blog post generated successfully.');
-  } catch (error) {
-    console.error(error);
-  }
+    const last20Titles = await Promise.all(last20TitlesPromises);
+
+    console.log(last20Titles);
+
+    // Generate a unique blog post using the OpenAI API
+    // const prompt = `Write a unique blog post about the same subject matter as the following 20 blog titles, but make sure it is different from them: ${last20Titles.join(', ')}\n\nTitle: {{title}}\nDate: {{date}}\nMeta Description: {{meta_description}}\nBlog Contents: {{blog_contents}}\n\nThe Blog Contents value should be an HTML string representing an insightful, well-written and confident blog structured with headings.`;
+
+    const promptTitle = `Come up with one SEO-friendly blog title that is similar to these: ${last20Titles.join(', ')}`
+
+    const openAITitleResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: promptTitle,
+      temperature: 0.9,
+      max_tokens: 750,
+      top_p: 1.0,
+    });
+
+    const outputTitle = openAITitleResponse.data.choices[0].text.replace(/^[\n\s"]+|[\n\s"]+$/g, '');
+
+    console.log(outputTitle);
+
+
+    const promptContents = `Now write 2-3 paragraphs of no more than 80 words, structured with HTML, and including headings and paragraphs. The blog must be based on this title: ${outputTitle}`;
+
+    const openaiContentsResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: promptContents,
+      temperature: 0.7,
+      max_tokens: 750,
+      top_p: 1.0,
+    });
+
+    // Extract JSON object from the generated text
+    const outputContent = openaiContentsResponse.data.choices[0].text;
+
+    console.log(outputContent);
+
+// ... (previous code remains the same)
+
+const slug = outputTitle.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "-");
+
+console.log(slug);
+
+// Save the blog post to the front-end repository
+const filePath = `src/blogs/${slug}.json`;
+
+// Create a new object with the required keys and values
+const blogPost = {
+  title: outputTitle,
+  date: new Date().toISOString(),
+  tags: [], // You can add tags here if required
+  category: "", // You can add a category here if required
+  contents: outputContent
 };
 
-// Schedule the generateBlogPost function to run every day at 12:00 AM
-cron.schedule('0 0 * * *', () => {
-  generateBlogPost('Artificial Intelligence');
+// Convert the object to a JSON string
+const jsonString = JSON.stringify(blogPost, null, 2);
+
+// Base64-encode the JSON string
+const base64EncodedContent = Buffer.from(jsonString).toString('base64');
+
+const createOrUpdateFileResponse = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+  owner: repoOwner,
+  repo: repoName,
+  path: filePath,
+  message: `Add new blog post: ${outputTitle}`,
+  content: base64EncodedContent,
+  branch: branch,
 });
 
-// Create Express app
-const app = express();
+console.log(`Successfully created/updated ${slug}.json in the ${repoName} repository.`);
 
-app.use(cors());
-app.use('/blogs', express.static('blogs'));
+// ... (rest of the code remains the same)
 
-// Define route to retrieve all blog posts
-app.get('/blog-posts', (req, res) => {
-  try {
-    // Get the contents of all JSON files in the blogs directory
-    const files = fs.readdirSync('blogs');
-    const blogPosts = files
-      .filter(file => file.endsWith('.json'))
-      .map(file => {
-        const fileContents = fs.readFileSync(`blogs/${file}`);
-        return JSON.parse(fileContents);
-      });
-    if (blogPosts.length > 0) {
-      res.json(blogPosts);
-    } else {
-      res.status(404).send('No blog posts found.');
-    }
+
+    console.log(`Successfully created/updated ${slug}.md in the ${repoName} repository.`);
+
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error.');
+    console.error('Error while generating and saving the blog post:', error);
   }
-});
+}
 
-// Define route to retrieve a specific blog post by ID
-app.get('/blog-posts/:id', (req, res) => {
-  const id = req.params.id;
-  try {
-    // Get the contents of the blog post file
-    const fileContents = fs.readFileSync(`blogs/${id}.json`);
-    const blogPost = JSON.parse(fileContents);
 
-    // Send the blog post as a response
-    res.json(blogPost);
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      res.status(404).send('Blog post not found.');
-    } else {
-      console.error(error);
-      res.status(500).send('Internal server error.');
-    }
-  }
-});
+// Call the function immediately when the server starts
+generateAndSaveBlogPost();
 
-app.get('*', (req, res) => {
-  res.send('OK');
-});
+// Set the daily cron job to run at 12 PM GMT
+cron.schedule('0 12 * * *', generateAndSaveBlogPost);
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000.');
+// Start the Express server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
